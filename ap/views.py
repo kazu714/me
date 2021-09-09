a400f1327265d2d6a75dd15ef2aed634e0e25a75
kazu714 <kazuhoh12@gmail.com>
2021-09-10 08:08:16 +0900

    first commit

---
+++ ap/views.py
@@ -0,0 +1,40 @@
+#<アプリ名>/views.py
+# ListViewとDetailViewを取り込み
+from django.views.generic import ListView, DetailView
+
+# 自分で作ったPostモデルを取り込み
+from .models import Post
+
+# 一覧
+class Index(ListView):
+    # 一覧するモデルを指定 -> `object_list`で取得可能
+    model = Post
+
+# 個別
+class Detail(DetailView):
+    # 詳細表示するモデルを指定 -> `object`で取得可能
+    model = Post
+    
+
+from django.views.generic.edit import CreateView, DeleteView
+
+class Create(CreateView):
+    model = Post
+    
+    # 編集対象にするフィールド
+    fields = ["title", "body",  "tags"]
+    
+
+from django.views.generic.edit import UpdateView, DeleteView, UpdateView
+
+class Update(UpdateView):
+    model = Post
+    fields = ["title", "body", "category", "tags"]
+    
+class Delete(DeleteView):
+    model = Post
+    success_url = "/"
+
+class Update(UpdateView):
+    model = Post
+    fields = ["title","body","category","tags"]
